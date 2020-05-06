1eccfe50b49ef39096dda3cf151129f1feaf06ed
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-06 11:19:02 -0700

    solution modified

--- solution.js
+++ solution.js
@@ -30,10 +30,11 @@ let decoderTable = {
 };
 
 // These are the inputs that you have
-let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10], [1, 17], [5, 7], [4, 0]];
-
-
+//let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10], [1, 17], [5, 7], [4, 0]];
+//let inputs =[[1, 8], 'BOP', [5, 7], [13, 2], [9, 13], [1, 4], 'BOP', [2, 1], [0, 1], [10, 10], [10, 9]];
+let inputs =[[9, 16], [13, 2], [13, 8], 'BOP', [2, 1], [0, 1], [10, 4], 'BOP', [2, 2],  [4, 11], 'BOP',  [4, 9], [8, 7], [7, 11], [3, 2], 'BOP', [11, 9], [3, 5], [0, 1], [7, 7], 'BOP', [13, 12], [13, 2], [8, 13], 'BOP', [5, 6], [12, 2], [9, 6], [20, 3]];
 // STEP 1: Make a new, empty array called letters.
+let letters = [];
 
 // STEP 2: Add the total number of beeps + boops to the letters array. This will
 // include looping over the inputs array to get the totals.
@@ -44,7 +45,17 @@ let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10],
 
 // HINT: Remember to deal with the 'BOP's! You can either ignore them or just pass them directly
 // into the letters array
-
+for(let i=0; i<inputs.length;i++)
+{
+  if(inputs[i]==='BOP')
+  {
+    letters.push('BOP');
+  }
+  else{letters.push(inputs[i][0] + inputs[i][1]);
+  }
+  
+}
+console.log(letters);
 
 // STEP 3: Stop and check. Let's print out our letters array and make sure it contains the
 // expected values! Uncomment the below code to check your answers.
@@ -59,7 +70,10 @@ let inputs = [[2, 6], [0, 5], [9, 3], [4, 8], [10, 5], 'BOP', [11, 12], [5, 10],
 
 // HINT: We need to loop over the letters array. For each item, we need to use it
 // as a key in the decoder table. Print the result!
-
+for(let i=0; i<letters.length;i++)
+{
+ console.log(decoderTable[letters[i]]);
+}
 
 
 
